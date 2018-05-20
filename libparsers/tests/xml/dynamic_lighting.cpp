d89faa074d1583aa61ffc9e4cad4fd76836bd467
Ben_ <bensujaam@gmail.com>
2018-05-20 17:28:23 +0200

    Fixed dynamic_lighting

--- libparsers/tests/xml/dynamic_lighting.cpp
+++ libparsers/tests/xml/dynamic_lighting.cpp
@@ -1,5 +1,6 @@
 #include "parsers/xml/dynamic_lighting.hpp"
 #include <doctest.h>
+#include <iostream>
 
 using namespace parsers::xml;
 using dynamic_lighting::lighting_info;
@@ -10,9 +11,9 @@ using namespace openbve2::datatypes;
 TEST_SUITE_BEGIN("libparsers - xml - dynamic_lighting");
 
 TEST_CASE("libparsers - xml - dynamic_lighting - test") {
-	const std::string test_value =
+	std::string const test_value =
 	    "<?xml version=\"1.0\" encoding=\"utf - 8\"?>"
-	    "<openBVE xmlns : xsi = \"http://www.w3.org/2001/XMLSchema-instance\" xmlns : xsd = "
+	    "<openBVE xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsd="
 	    "\"http://www.w3.org/2001/XMLSchema\">"
 	    "<Brightness>"
 	    "<Time>10.00</Time>"
@@ -31,24 +32,63 @@ TEST_CASE("libparsers - xml - dynamic_lighting - test") {
 	    "</openBVE>";
 
 	parsers::errors::multi_error_t output_errors;
-	const std::vector<lighting_info> output =
-	    dynamic_lighting::parse("some_file.xml", test_value, output_errors);
+	auto const output = dynamic_lighting::parse("some_file.xml", test_value, output_errors);
 
+	// The if here is needed to check if there's any errors to report
+	// Because otherwise it would print the filename
 	if (!output_errors.empty()) {
-		INFO(output_errors);
-		REQUIRE(output_errors.empty());
+		std::cout << output_errors;
 	}
+	REQUIRE(output_errors.empty());
 
 	// Check that the struct values match
 	CHECK_EQ(output[0].ambient, color8_rgb{1, 1, 1});
 	CHECK_EQ(output[0].cablighting, 80);
 	CHECK_EQ(output[0].directional_lighting, color8_rgb{1, 1, 1});
 	CHECK_EQ(output[0].light_direction, glm::vec3{1.0, 1.0, 1.0});
-	CHECK_EQ(output[0].time, 0);
+	CHECK_EQ(output[0].time, 3600 * 10);
+
+	CHECK_EQ(output[1].ambient, color8_rgb{160, 160, 160});
+	CHECK_EQ(output[1].cablighting, 255);
+	CHECK_EQ(output[1].directional_lighting, color8_rgb{160, 160, 160});
+	CHECK_EQ(output[1].light_direction, glm::vec3{-0.5f, -0.5f, -0.5f});
+	CHECK_EQ(output[1].time, 3600 * 12);
+}
+
+TEST_CASE("libparsers - xml - dynamic_lighting - insane_values") {
+	std::string const test_value =
+	    "<?xml version=\"1.0\" encoding=\"utf - 8\"?>"
+	    "<openBVE xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsd="
+	    "\"http://www.w3.org/2001/XMLSchema\">"
+	    "<SomeOtherTag>" // This is not <Brightness>, so that the parser doesn't read this by
+	                     // accident
+	    "<Time>sghkjthj</Time>"
+	    "<AmbientLight>text</AmbientLight>"
+	    "<DirectionalLight>1,</DirectionalLight>"
+	    "<LightDirection>-15gfajhk</LightDirection>"
+	    "<CabLighting>5000000000</CabLighting>"
+	    "<UselessTag>someuselessvalue</UselessTag>"
+	    "</SomeOtherTag>"
+	    "<Brightness>"
+	    "<Time>sghkjthj</Time>"
+	    "<AmbientLight>text</AmbientLight>"
+	    "<DirectionalLight>1,</DirectionalLight>"
+	    "<LightDirection>sdjgh57nfd, hfjidr, dsguah</LightDirection>"
+	    "<CabLighting>5000000000</CabLighting>"
+	    "<UselessTag>someuselessvalue</UselessTag>"
+	    "</Brightness>"
+	    "</openBVE>";
+
+	parsers::errors::multi_error_t output_errors;
+	auto const output = dynamic_lighting::parse("some_file.xml", test_value, output_errors);
+
+	REQUIRE_EQ(output_errors["some_file.xml"].size(), 4);
 
 	CHECK_EQ(output[0].ambient, color8_rgb{160, 160, 160});
-	CHECK_EQ(output[0].cablighting, 255);
+	CHECK_EQ(output[0].cablighting, 0);
 	CHECK_EQ(output[0].directional_lighting, color8_rgb{160, 160, 160});
-	CHECK_EQ(output[0].light_direction, glm::vec3{-0.5, -0.5, -0.5});
-	CHECK_EQ(output[0].time, 12);
+	CHECK_EQ(output[0].light_direction, glm::vec3{-0.223645424f, -0.866025404f, 0.44719428f});
+	CHECK_EQ(output[0].time, 0);
 }
+
+TEST_SUITE_END();
