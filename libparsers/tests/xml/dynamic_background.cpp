9183b8a06c025dea62380bea6ab826e53bb1c653
KevinCybura <KevinCybura@gmail.com>
2018-05-29 15:50:30 -0400

    05/29/2018 dynamic_background parser tests written

--- libparsers/tests/xml/dynamic_background.cpp
+++ libparsers/tests/xml/dynamic_background.cpp
@@ -14,7 +14,7 @@ TEST_CASE("libparser - xml - dynamic_background - test single backgound") {
 	// clang-format off
     std::string const test_single_background = 
         "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-        "<openBVE>"
+        "<openBVE xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
             "<Background>"
                 "<Time>00.00</Time>"
                 "<Mode>FadeIn</Mode>"
@@ -24,32 +24,33 @@ TEST_CASE("libparser - xml - dynamic_background - test single backgound") {
         "</openBVE>";
 	// clang-format on
 	parsers::errors::multi_error_t output_errors;
-	auto lm = [](std::string const& base, std::string const& relative) {
+	auto rel_file_func = [](std::string const& base, std::string const& relative) {
 		return base + "/" + relative;
 	};
-	auto const output = dynamic_background::parse("some_file.xml", test_single_background, output_errors, lm);
+	auto const output = dynamic_background::parse("some_file.xml", test_single_background,
+	                                              output_errors, rel_file_func);
 
 	if (!output_errors.empty()) {
 		std::cout << output_errors;
 	}
 	REQUIRE(output_errors.empty());
-    
-    REQUIRE(output.is<std::vector<dynamic_background::texture_background_info>>());
-    auto& background = output.get_unchecked<std::vector<dynamic_background::texture_background_info>>();
-    CHECK_EQ(background.size(), 1);
+
+	REQUIRE(output.is<std::vector<dynamic_background::texture_background_info>>());
+	auto& background =
+	    output.get_unchecked<std::vector<dynamic_background::texture_background_info>>();
+	CHECK_EQ(background.size(), 1);
 
 	CHECK_EQ(background[0].time, 0);
 	CHECK_EQ(background[0].repetitions, 6);
 	CHECK_EQ(background[0].transition_mode, dynamic_background::texture_background_info::fade_in);
 	CHECK_EQ(background[0].filename, "some_file.xml/Cloudy.png");
-
 }
 
 TEST_CASE("libparser - xml - dynamic_background - test multiple backgound") {
 	// clang-format off
     std::string const test_multiple_backgrounds = 
         "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-        "<openBVE>"
+        "<openBVE xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
             "<Background>"
                 "<Time>00.00</Time>"
                 "<Mode>FadeIn</Mode>"
@@ -67,10 +68,11 @@ TEST_CASE("libparser - xml - dynamic_background - test multiple backgound") {
 	// clang-format on
 
 	parsers::errors::multi_error_t output_errors;
-	auto lm = [](std::string const& base, std::string const& relative) {
+	auto rel_file_func = [](std::string const& base, std::string const& relative) {
 		return base + "/" + relative;
 	};
-	auto const output = dynamic_background::parse("some_file.xml", test_multiple_backgrounds, output_errors, lm);
+	auto const output = dynamic_background::parse("some_file.xml", test_multiple_backgrounds,
+	                                              output_errors, rel_file_func);
 
 	if (!output_errors.empty()) {
 		std::cout << output_errors;
@@ -92,6 +94,59 @@ TEST_CASE("libparser - xml - dynamic_background - test multiple backgound") {
 	CHECK_EQ(vec[1].transition_mode, dynamic_background::texture_background_info::fade_out);
 	CHECK_EQ(vec[1].filename, "some_file.xml/Sunny.png");
 	CHECK_EQ(vec[1].transition_time, 10);
+}
 
+TEST_CASE("libparser - xml - dynamic_background - object_background") {
+	// clang-format off
+    std::string const test_object_background =
+        "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+            "<openBVE xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+                "<Background>"
+                    "<Object>Background.csv</Object>"
+                "</Background>"
+            "</openBVE>";
+	// clang-format on
 
+	parsers::errors::multi_error_t output_errors;
+	auto rel_file_func = [](std::string const& base, std::string const& relative) {
+		return base + "/" + relative;
+	};
+	auto const output = dynamic_background::parse("some_file.xml", test_object_background,
+	                                              output_errors, rel_file_func);
+
+	if (!output_errors.empty()) {
+		std::cout << output_errors;
+	}
+	REQUIRE(output_errors.empty());
+
+	REQUIRE(output.is<dynamic_background::object_background_info>());
+	auto& object_background = output.get_unchecked<dynamic_background::object_background_info>();
+
+	CHECK_EQ(object_background.filename, "some_file.xml/Background.csv");
+}
+
+TEST_CASE("libparser - xml - dynamic_background - should allow only one object_background") {
+	// clang-format off
+    std::string const test_object_background =
+        "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+            "<openBVE xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">"
+                "<Background>"
+                    "<Object>Background.csv</Object>"
+                "</Background>"
+                "<Background>"
+                    "<Object>Background2.csv</Object>"
+                "</Background>"
+            "</openBVE>";
+	// clang-format on
+
+	parsers::errors::multi_error_t output_errors;
+	auto rel_file_func = [](std::string const& base, std::string const& relative) {
+		return base + "/" + relative;
+	};
+	auto const output = dynamic_background::parse("some_file.xml", test_object_background,
+	                                              output_errors, rel_file_func);
+
+	REQUIRE(!output_errors.empty());
+	CHECK_EQ(output_errors["some_file.xml"][0].error,
+	         "Multiple Object backgrounds: only one object background is allowed.");
 }
