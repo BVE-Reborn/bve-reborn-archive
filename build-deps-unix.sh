9dcc6c337e4448a12bce4f2bddfceb8c8c41bcde
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2018-08-22 09:50:15 -0400

    Squashed commit of the following:
    commit dfde4aab355a9248ca6f602150e45769213ff32f
    Author: Connor Fitzgerald <connorwadefitzgerald@gmail.com>
    Date:   Tue Aug 21 22:54:19 2018 -0400
    
        Make l/asan suppression path relative to src dir
    
    commit 34f2696369c9bc9dfd9f2ff00155859ad36a6eb7
    Author: Connor Fitzgerald <connorwadefitzgerald@gmail.com>
    Date:   Tue Aug 21 22:40:03 2018 -0400
    
        Change lsan suppression path
    
    commit 6c6fe1904a666d2f44d600fc5a9310ad4ee475b2
    Author: Connor Fitzgerald <connorwadefitzgerald@gmail.com>
    Date:   Tue Aug 21 22:24:43 2018 -0400
    
        Add lsan and ubsan suppressions
    
    commit a85a5f68aa3ccff6575a87c1c752dc52e8efa3d8
    Author: Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
    Date:   Tue Aug 21 21:22:30 2018 -0400
    
        Fix clang boost build again
    
    commit 89c92d774d33a7bc426b3a57343ddba9859dd6eb
    Author: Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
    Date:   Tue Aug 21 21:20:56 2018 -0400
    
        Fix clang boost build
    
    commit fba5ec384560b8ca7d6c96a55be5c8092dc58364
    Author: Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
    Date:   Tue Aug 21 21:07:30 2018 -0400
    
        Add clang specific boost build on travis-ci

--- build-deps-unix.sh
+++ build-deps-unix.sh
@@ -22,14 +22,19 @@ else
 	linking=shared
 fi
 
-# # Get Newer CMake
 mkdir -p dependencies/binary/{downloads,build_x86,build_x64,src/boost}
 wget -c -nc https://bsfproxy.connorwfitzgerald.com/bver/boost_1_64_0.tar.bz2 -O dependencies/binary/downloads/boost_1_64_0.tar.bz2
 
-# # Build Boost.... 
+# Build Boost.... 
 tar xjf dependencies/binary/downloads/boost_1_64_0.tar.bz2 -C dependencies/binary/src/boost
 cd dependencies/binary/src/boost/boost_1_64_0/
-echo "using gcc : 7.1 : `type -p g++` ; " > tools/build/src/user-config.jam
-./bootstrap.sh --prefix=$basedir/dependencies/binary/build_x64 --with-libraries=filesystem,system,regex
-./b2 link=$linking threading=multi variant=release runtime-link=shared install -j$(($(nproc) + 1)) -d0
+if [ $# -ge 1 ]; then 
+	echo "using clang : 6.0 : `type -p clang++-6.0` ; " > tools/build/src/user-config.jam
+	./bootstrap.sh --with-toolset=clang --prefix=$basedir/dependencies/binary/build_x64 --with-libraries=filesystem,system,regex
+	./b2 toolset=clang link=$linking threading=multi variant=release runtime-link=shared install -j$(($(nproc) + 1)) -d0
+else
+	echo "using gcc : 7.1 : `type -p g++` ; " > tools/build/src/user-config.jam
+	./bootstrap.sh --prefix=$basedir/dependencies/binary/build_x64 --with-libraries=filesystem,system,regex
+	./b2 link=$linking threading=multi variant=release runtime-link=shared install -j$(($(nproc) + 1)) -d0
+fi
 cd "${0%/*}"
