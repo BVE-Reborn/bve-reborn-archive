a629f829c6c10e9f2d3942e3531c5d8c2510ffc7
Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
2018-09-10 03:58:49 -0400

    Resolve ssh key decryption

--- tools/travis/deploy_docs.sh
+++ tools/travis/deploy_docs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 tools/travis/travis_ci_rsa # Allow read access to the private key
-ssh-add tools/travis/travis_ci_rsa # Add the private key to SSH
+ls ${TRAVIS_BUILD_DIR}/tools/travis/
+chmod 600 ${TRAVIS_BUILD_DIR}/tools/travis/travis_ci_rsa # Allow read access to the private key
+ssh-add ${TRAVIS_BUILD_DIR}/tools/travis/travis_ci_rsa # Add the private key to SSH
 
 ssh $DEPLOY_USER@docs.bvereborn.com "echo sup"
