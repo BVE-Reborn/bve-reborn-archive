d613bfba366b0c1a7f7370780b2a22305b7fdb7a
Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
2018-09-10 04:03:41 -0400

    fix travis-ci deploy script

--- tools/travis/deploy_docs.sh
+++ tools/travis/deploy_docs.sh
@@ -5,4 +5,4 @@ ls ${TRAVIS_BUILD_DIR}/tools/travis/
 chmod 600 ${TRAVIS_BUILD_DIR}/tools/travis/travis_ci_rsa # Allow read access to the private key
 ssh-add ${TRAVIS_BUILD_DIR}/tools/travis/travis_ci_rsa # Add the private key to SSH
 
-ssh $DEPLOY_USER@docs.bvereborn.com "echo sup"
+ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@docs.bvereborn.com "echo sup"
