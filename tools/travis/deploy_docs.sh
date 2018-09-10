cb3b897718b2596cf0d7ddcf4e7de463171c9746
Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
2018-09-10 04:17:09 -0400

    Enable deploying of docs

--- tools/travis/deploy_docs.sh
+++ tools/travis/deploy_docs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-ls ${TRAVIS_BUILD_DIR}/tools/travis/
 chmod 600 ${TRAVIS_BUILD_DIR}/tools/travis/travis_ci_rsa # Allow read access to the private key
 ssh-add ${TRAVIS_BUILD_DIR}/tools/travis/travis_ci_rsa # Add the private key to SSH
 
 ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@docs.bvereborn.com "echo sup"
+rsync -e "ssh -o StrictHostKeyChecking=no" -az ${TRAVIS_BUILD_DIR}/../docs-build/html/ $DEPLOY_USER@docs.bvereborn.com:$DEPLOY_LOCATION/$TRAVIS_BRANCH/
