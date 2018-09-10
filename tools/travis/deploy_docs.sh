3ea6cf67e99a0ed535a607d4ac1b63caad1d67f4
Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
2018-09-10 03:50:33 -0400

    Fix travis docs deploy config

--- tools/travis/deploy_docs.sh
+++ tools/travis/deploy_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 tools/travis/travis_ci_rsa.enc # Allow read access to the private key
-ssh-add tools/travis/travis_ci_rsa.enc # Add the private key to SSH
+chmod 600 tools/travis/travis_ci_rsa # Allow read access to the private key
+ssh-add tools/travis/travis_ci_rsa # Add the private key to SSH
 
 ssh $DEPLOY_USER@docs.bvereborn.com "echo sup"
