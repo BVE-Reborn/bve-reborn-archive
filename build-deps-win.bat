ff67cfcea160a09f8794827f2c8f4db9cbfe2961
Connor Wade Fitzgerald <connorwadefitzgerald@gmail.com>
2018-09-11 01:25:28 -0400

    Documentation Overhall

--- build-deps-win.bat
+++ build-deps-win.bat
@@ -1,5 +1,5 @@
 cd dependencies\binary\src\boost\boost_1_64_0\
 call bootstrap.bat
-b2 install --prefix=..\..\..\build_x86 --with-filesystem --with-system --with-regex variant=debug,release link=shared threading=multi address-model=32 toolset=msvc -d0
+rem b2 install --prefix=..\..\..\build_x86 --with-filesystem --with-system --with-regex variant=debug,release link=shared threading=multi address-model=32 toolset=msvc -d0
 b2 install --prefix=..\..\..\build_x64 --with-filesystem --with-system --with-regex variant=debug,release link=shared threading=multi address-model=64 toolset=msvc -d0
 cd ..\..\..\..\..
---
+++ docs/development.md
@@ -0,0 +1,6 @@
+Development Guide {#Development}
+===============
+
+Here are all of the guides relating to building stuff with the api's that BVE-Reborn provides, as well as guides for helping contribute to BVE-Reborn.
+
+\subpage building
