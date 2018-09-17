4169989ff7fd98364b2a9fbd38148378ca3a982c
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2018-09-17 01:33:40 -0400

    Remove boost building from travis-ci
    Former-commit-id: 7ac63d5a31d8ff27ce34a6b68d8ca8a4ed330e5d

--- tools/travis/linux_deps_clang.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wget -q https://bsfproxy.connorwfitzgerald.com/bver/cmake-3.12.1-Linux-x86_64.sh
-mkdir -p $HOME/binary_root
-bash cmake-3.12.1-Linux-x86_64.sh --skip-license --prefix=$HOME/binary_root --exclude-subdir
-$HOME/binary_root/bin/cmake --version
-bash ./build-deps-unix.sh clang
