30254731863e782786bff8806118a86bcc28ecc1
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-01-29 06:59:22 -0500

    Switch to jenkins as primary CI
    Former-commit-id: 6c91da091a6833558c02ff4652c888fe1f35d816

--- util/travis/deploy_docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ${TRAVIS_BUILD_DIR}/util/travis/travis_ci_rsa # Allow read access to the private key
-ssh-add ${TRAVIS_BUILD_DIR}/util/travis/travis_ci_rsa # Add the private key to SSH
-
-rsync -e "ssh -o StrictHostKeyChecking=no" -az --delete ${TRAVIS_BUILD_DIR}/../docs-build/html/ $DEPLOY_USER@docs.bvereborn.com:$DEPLOY_LOCATION/$TRAVIS_BRANCH/
