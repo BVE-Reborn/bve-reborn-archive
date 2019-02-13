c973ebb912c959e7f79595679f006c1cdb5bedee
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-02-12 19:57:13 -0500

    doxygen: fix input paths
    Former-commit-id: cba02bd8e5cd585938623b061821d0e5e2c6f98c

--- docs.sh
+++ docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -lt 1 ]; then
 	echo "Must provide destination argument"
 	exit 1
@@ -12,4 +14,5 @@ mkdir -p "$DEST"
 echo "Doxygen: $(doxygen --version)"
 cp "docs/config/Doxyfile" "$DEST/Doxyfile"
 echo "OUTPUT_DIRECTORY = $DEST" >> "$DEST/Doxyfile"
+echo "INPUT = " "${PWD}"/{docs,lib,src} >> "$DEST/Doxyfile"
 doxygen "$DEST/Doxyfile"
