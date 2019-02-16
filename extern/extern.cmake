8535d10cd562f209444b6e1031116a77c0cb84f1
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-02-16 06:30:26 -0500

    Add concurrent-queue target in cmake
    Former-commit-id: 97a7caf4890cfc06874968f90b1c90e1b300c299

--- extern/extern.cmake
+++ extern/extern.cmake
@@ -1,33 +1,33 @@
 add_library(nova::glm INTERFACE IMPORTED)
 set_target_properties(nova::glm PROPERTIES
 	INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/nova/3rdparty/glm"
-	INTERFACE_COMPILE_FEATURES cxx_std_17
 	INTERFACE_COMPILE_DEFINITIONS "GLM_ENABLE_EXPERIMENTAL"
 )
 
 add_library(mapbox::variant INTERFACE IMPORTED)
 set_target_properties(mapbox::variant PROPERTIES
 	INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/mapbox-variant/include"
-	INTERFACE_COMPILE_FEATURES cxx_std_17
 )
 
 add_library(rapidxml::rapidxml INTERFACE IMPORTED)
 set_target_properties(rapidxml::rapidxml PROPERTIES
 	INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/rapidxml_ns"
-	INTERFACE_COMPILE_FEATURES cxx_std_17
 )
 
 add_library(doctest::doctest INTERFACE IMPORTED)
 set_target_properties(doctest::doctest PROPERTIES
 	INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/doctest/doctest"
-	INTERFACE_COMPILE_FEATURES cxx_std_17
 )
 
 add_library(gsl::gsl INTERFACE IMPORTED)
 set_target_properties(gsl::gsl PROPERTIES
 	INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/gsl/include"
-	INTERFACE_COMPILE_FEATURES cxx_std_17
 	INTERFACE_COMPILE_DEFINITIONS "GSL_THROW_ON_CONTRACT_VIOLATION"
+)
+
+add_library(ccq::concurrentqueue INTERFACE IMPORTED)
+set_target_properties(ccq::concurrentqueue PROPERTIES
+	INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/concurrentqueue"
 )
 
 function(make_target_includes_system target)
