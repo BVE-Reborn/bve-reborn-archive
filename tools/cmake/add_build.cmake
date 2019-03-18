c3ff8184c9ae118e0e2b2a431dc053b70274b298
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-03-18 00:34:05 -0400

    [cmake] Properly call add_library
    Former-commit-id: 51e031ff327a01b6f2b9ca599fcd8ffa569d925f

--- tools/cmake/add_build.cmake
+++ tools/cmake/add_build.cmake
@@ -1,14 +1,20 @@
 function(add_bve_executable _target)
 	set(_args ${ARGN})
 
-	add_executable(${_target} ${_args})
+	if(ARGC GREATER_EQUAL 2)
+		if(${ARGV1} MATCHES "STATIC|SHARED|OBJECT|WIN32|PUBLIC|PRIVATE|INTERFACE")		
+			set(_type ${ARGV1})
+		endif()
+	endif()
+	
+	list(REMOVE_ITEM _args STATIC SHARED OBJECT WIN32 PUBLIC PRIVATE INTERFACE)
+
+	add_executable(${_target} ${_type} ${_args})
 	
 	target_add_sanitizers(${_target})
 	target_add_warnings(${_target})
 	target_bve_coverage(${_target})
 	target_format(${_target})
-
-	list(REMOVE_ITEM _args STATIC SHARED OBJECT WIN32 PUBLIC PRIVATE INTERFACE)
 
 	source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${_args})
 endfunction()
@@ -16,14 +22,20 @@ endfunction()
 function(add_bve_library _target)
 	set(_args ${ARGN})
 
-	add_library(${_target} ${_args})
+	if(ARGC GREATER_EQUAL 2)
+		if(${ARGV1} MATCHES "STATIC|SHARED|OBJECT|WIN32|PUBLIC|PRIVATE|INTERFACE")		
+			set(_type ${ARGV1})
+		endif()
+	endif()
+	
+	list(REMOVE_ITEM _args STATIC SHARED OBJECT WIN32 PUBLIC PRIVATE INTERFACE)
+
+	add_library(${_target} ${_type} ${_args})
 	
 	target_add_sanitizers(${_target})
 	target_add_warnings(${_target})
 	target_bve_coverage(${_target})
 	target_format(${_target})
-
-	list(REMOVE_ITEM _args STATIC SHARED OBJECT WIN32 PUBLIC PRIVATE INTERFACE)
 
 	source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${_args})
 endfunction()
