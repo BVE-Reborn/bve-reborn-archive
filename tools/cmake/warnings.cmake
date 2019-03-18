4ab6a57d974bbd8e0962d8c3aaa9144ad06d0ce0
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-03-18 01:42:28 -0400

    [cmake] Add linux warnings
    Former-commit-id: e3e3c6057a1e072d5b1db7a135ff603b38ab7584

--- tools/cmake/warnings.cmake
+++ tools/cmake/warnings.cmake
@@ -1,7 +1,7 @@
 function(target_add_warnings target)
 	if (OS_WINDOWS)
-		target_compile_options_if_supported(${target} PUBLIC /W4)
-		target_compile_options_if_supported(${target} PUBLIC 
+		target_compile_options_if_supported(${target} PRIVATE /W4)
+		target_compile_options_if_supported(${target} PRIVATE 
 			/w44061
 			/w44062
 			/w44263
@@ -17,10 +17,42 @@ function(target_add_warnings target)
 			/w44668
 			/wd4251
 		)
-		target_compile_options_if_supported(${target} PUBLIC /experimental:external /external:anglebrackets /external:W3)
-		target_compile_options_if_supported(${target} PUBLIC /WX)
-		target_compile_options_if_supported(${target} PUBLIC /permissive-)
+		target_compile_options_if_supported(${target} PRIVATE /experimental:external /external:anglebrackets /external:W3)
+		target_compile_options_if_supported(${target} PRIVATE /WX)
+		target_compile_options_if_supported(${target} PRIVATE /permissive-)
 	else()
-
+		target_compile_options_if_supported(${target} PRIVATE 
+			-fdiagnostics-color=always
+			-Wall
+			-Wcast-align
+			-Wcast-qual
+			-Wconversion
+			-Wctor-dtor-privacy
+			-Wdisabled-optimization
+			-Wdouble-promotion
+			-Wduplicated-branches
+			-Wduplicated-cond
+			-Wextra
+			-Wformat=2
+			-Winit-self
+			-Wlogical-op
+			-Wno-gnu-zero-variadic-macro-arguments
+			-Wmissing-include-dirs
+			-Wno-sign-conversion
+			-Wnoexcept
+			-Wnull-dereference
+			-Wold-style-cast
+			-Woverloaded-virtual
+			-Wpedantic
+			-Wrestrict
+			-Wshadow
+			-Wstrict-aliasing=1
+			-Wstrict-null-sentinel
+			-Wstrict-overflow=2
+			-Wswitch-default
+			-Wundef
+			-Wno-unknown-pragmas
+			-Wuseless-cast
+		) 
 	endif()
 endfunction()
