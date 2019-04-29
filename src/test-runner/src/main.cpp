d197b1a999b2536538e9f1c50082dd6b59ec2e9e
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-04-29 04:19:34 -0400

    [tests] Add logger to test executable
    Former-commit-id: 8797e789a6c4c5f25f0a13275ebe329d43b3a86a

--- src/test-runner/src/main.cpp
+++ src/test-runner/src/main.cpp
@@ -1,4 +1,40 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 // ReSharper disable CppUnusedIncludeDirective
 #include <doctest/doctest.h>
 // ReSharper restore CppUnusedIncludeDirective
+
+#include <foundational/logging/logger.hpp>
+
+class StandardOutBackend final : foundational::logging::LoggerBackend {
+  public:
+	StandardOutBackend() : LoggerBackend(){};
+	void write(foundational::logging::Level /*log_level*/,
+	           foundational::logging::Timestamp const& /*time*/,
+	           std::string const& /*source*/,
+	           std::string const& formatted_content,
+	           std::string const& /*raw_content*/) override {
+		std::cout << formatted_content;
+	}
+	void flush() override {
+		std::cout << std::flush;
+	}
+	~StandardOutBackend() {
+		flushLog();
+	}
+};
+
+FOUNDATIONAL_LOGGER_BACKEND(StandardOutBackend, stdout_back);
+
+int main() {
+	foundational::statics::StaticGlobals::initialize();
+	atexit(foundational::statics::StaticGlobals::deinitialize);
+
+	doctest::Context context;
+
+	int const res = context.run(); // run
+
+	if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+		return res;           // propagate the result of the tests
+
+	return res; // the result from doctest is propagated here as well
+}
