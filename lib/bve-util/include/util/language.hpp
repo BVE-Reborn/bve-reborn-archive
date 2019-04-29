1872f4f56a74a5f9f173f506c465f386d79ae85c
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-04-29 04:15:49 -0400

    [util] Add various "language" features/typedefs/macros
    Former-commit-id: 2d5839ba0bca85dfbdeeff85db07563a5f1f247a

---
+++ lib/bve-util/include/util/language.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cinttypes>
+#include <immintrin.h>
+
+// ReSharper disable CppInconsistentNaming
+#define cast static_cast
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+using iSize = std::ptrdiff_t;
+using i128 = __m128i;
+using i256 = __m256i;
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using uSize = std::size_t;
+
+using f32 = float;
+using f64 = double;
+using f80 = long double;
+// ReSharper restore CppInconsistentNaming
