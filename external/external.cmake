1324c6a4470bcb3f4db898630b532b3d9fe6268a
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-06-26 00:14:37 -0400

    [all] Remove nova and swig tests

--- external/external.cmake
+++ external/external.cmake
@@ -4,32 +4,12 @@ set(VCPKG_INSTALLED_PATH_DEBUG "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/
 # Thread
 find_package(Threads REQUIRED)
 
-# NOVA
-find_package(glm CONFIG REQUIRED)
-find_package(nlohmann_json CONFIG REQUIRED)
-find_package(unofficial-vulkan-memory-allocator CONFIG REQUIRED)
-find_package(minitrace CONFIG REQUIRED)
-find_package(fmt CONFIG REQUIRED)
-find_package(miniz CONFIG REQUIRED)
-find_package(glslang CONFIG REQUIRED)
-find_package(spirv_cross_core CONFIG REQUIRED)
-find_package(spirv_cross_glsl CONFIG REQUIRED)
-find_package(spirv_cross_cpp CONFIG REQUIRED)
-find_package(spirv_cross_hlsl CONFIG REQUIRED)
-find_package(spirv_cross_msl CONFIG REQUIRED)
-find_package(spirv_cross_reflect CONFIG REQUIRED)
-find_package(spirv_cross_util CONFIG REQUIRED)
-find_package(SPIRV-Tools CONFIG REQUIRED)
-find_package(Vulkan REQUIRED)
-
-find_package(foundational CONFIG REQUIRED)
-find_package(nova-renderer CONFIG REQUIRED)
-
 # BVE
 find_package(CLI11 CONFIG REQUIRED)
 find_package(cppfs CONFIG REQUIRED)
 find_package(doctest CONFIG REQUIRED)
 find_package(fmt CONFIG REQUIRED)
+find_package(foundational CONFIG REQUIRED)
 find_package(glm CONFIG REQUIRED)
 find_package(unofficial-abseil CONFIG REQUIRED)
 
--- libs/bve-core/extras/swig/bve-core.i
+++
@@ -1,9 +0,0 @@
-%module bve_core_python
-
-%{
-#define SWIG_FILE_WTIH_INIT
-#include <core/openbve/filesystem.hpp>
-#include <util/unlanguage.hpp>
-%}
-
-%include <core/openbve/filesystem.hpp>
