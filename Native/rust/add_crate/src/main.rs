cf3d33be4fc57860482388da33abd5596e8b2ec6
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-07-12 21:35:41 -0400

    [rust-tools] Finish add_crate utility

--- Native/rust/add_crate/src/main.rs
+++ Native/rust/add_crate/src/main.rs
@@ -1,14 +1,71 @@
+#[deny(rust_2018_idioms)]
+
 use clap::{Arg, App};
 use std::process::{Command, exit};
 use std::env::current_dir;
 use std::fs::File;
 use std::io;
-use std::io::{Read, ErrorKind, Write};
+use std::io::{ErrorKind, Write};
 use toml::Value;
-use toml::map::{Keys, Map};
+use toml::map::Map;
 use toml::ser::Serializer;
 use serde::ser::Serialize;
 use std::error::Error;
+use std::path::{PathBuf, Path};
+use std::ops::Div;
+
+struct PathWrapper {
+    path: PathBuf,
+}
+
+impl PathWrapper {
+    pub fn new() -> PathWrapper {
+        PathWrapper {
+            path: PathBuf::new()
+        }
+    }
+
+    pub fn to_path(&self) -> &Path {
+        &*self.path.as_path()
+    }
+}
+
+impl From<&Path> for PathWrapper {
+    fn from(path: &Path) -> Self {
+        PathWrapper {
+            path: path.to_path_buf(),
+        }
+    }
+}
+
+impl From<PathBuf> for PathWrapper {
+    fn from(path_buf: PathBuf) -> Self {
+        PathWrapper {
+            path: path_buf,
+        }
+    }
+}
+
+impl Div<&str> for PathWrapper {
+    type Output = PathWrapper;
+
+    fn div(mut self, rhs: &str) -> Self::Output {
+        self.path.push(rhs);
+        self
+    }
+}
+
+impl Div<&str> for &PathWrapper {
+    type Output = PathWrapper;
+
+    fn div(self, rhs: &str) -> Self::Output {
+        let mut tmp = self.path.clone();
+        tmp.push(rhs);
+        PathWrapper {
+            path: tmp
+        }
+    }
+}
 
 fn run_cargo(name: &str) {
     let mut command =
@@ -36,44 +93,73 @@ fn run_cargo(name: &str) {
     }
 }
 
-fn add_to_cbindgen(name: &str) -> io::Result<()> {
-    let mut buf = current_dir()?.to_path_buf();
-    buf.push("cbindgen-meta");
-    buf.push("Cargo.toml");
+fn add_to_cbindgen_meta(name: &str) -> io::Result<()> {
+    let buf = PathWrapper::from(current_dir()?) / "cbindgen-meta" / "Cargo.toml";
 
-    let mut cargo_toml = File::open(buf.as_path())?;
-    let mut contents = String::new();
-    cargo_toml.read_to_string(&mut contents)?;
+    let contents = std::fs::read_to_string(buf.to_path()).unwrap();
 
     let mut parsed = match contents.parse::<Value>() {
-        Ok(value) => value,
-        Err(err) => return Err(std::io::Error::new(ErrorKind::InvalidData, err.to_string()))
+        Ok(value) =>
+            value,
+        Err(err) =>
+            return Err(io::Error::new(ErrorKind::InvalidData, err.to_string()))
     };
 
-    let mut parent = parsed["dependencies"].as_table_mut().unwrap();
+    let parent = parsed["dependencies"].as_table_mut().unwrap();
     let mut addition = Map::new();
     addition.insert(String::from("path"), Value::String(format!("../{}", name)));
     parent.insert(name.to_owned(), Value::Table(addition));
 
-    println!("{:#?}", parsed);
-
     let mut contents = String::new();
     parsed.serialize(&mut Serializer::pretty(&mut contents)).expect("Error reconstructing string");
 
-    let mut cargo_toml = File::create(buf.as_path())?;
+    let mut cargo_toml = File::create(buf.to_path())?;
     match cargo_toml.write(contents.as_bytes()) {
         Ok(bytes) => {
-            println!("wrote {} bytes", bytes);
+            println!("wrote {} bytes to {:?}", bytes, buf.to_path());
         }
         Err(err) => {
-            eprintln!("Error writing {}: {}", buf.to_str().unwrap(), err.description())
+            eprintln!("Error writing {}: {}", buf.path.to_str().unwrap(), err.description());
+            exit(1);
         }
     };
 
     Ok(())
 }
 
-fn main() {
+fn add_to_cbindgen(name: &str) -> io::Result<()> {
+    let buf = PathWrapper::from(current_dir()?) / "cbindgen.toml";
+
+    let contents = std::fs::read_to_string(buf.to_path()).unwrap();
+    let mut parsed = match contents.parse::<Value>() {
+        Ok(value) =>
+            value,
+        Err(err) =>
+            return Err(io::Error::new(ErrorKind::InvalidData, err.to_string()))
+    };
+
+    let parent = parsed["parse"].as_table_mut().unwrap();
+    let member = parent["extra_bindings"].as_array_mut().unwrap();
+    member.push(Value::String(name.to_owned()));
+
+    let mut contents = String::new();
+    parsed.serialize(&mut Serializer::pretty(&mut contents)).expect("Error reconstructing string");
+
+    let mut cargo_toml = File::create(buf.to_path())?;
+    match cargo_toml.write(contents.as_bytes()) {
+        Ok(bytes) => {
+            println!("wrote {} bytes to {:?}", bytes, buf.to_path());
+        }
+        Err(err) => {
+            eprintln!("Error writing {}: {}", buf.path.to_str().unwrap(), err.description());
+            exit(1);
+        }
+    };
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
     let matches =
         App::new("bve-reborn add crate utility")
             .version("0.1")
@@ -86,6 +172,17 @@ fn main() {
             .get_matches();
 
     let name = matches.value_of("NAME").unwrap();
+
+    let result = PathWrapper::from(current_dir().unwrap()) / name;
+    let path = result.to_path();
+
+    if path.exists() {
+        eprintln!("Crate {} already exists", name);
+        exit(0);
+    }
+
     run_cargo(name);
-    add_to_cbindgen(name);
+    add_to_cbindgen(name)?;
+    add_to_cbindgen_meta(name)?;
+    Ok(())
 }
